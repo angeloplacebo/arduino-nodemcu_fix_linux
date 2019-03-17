cc94167f9dfbf1d3c1541b36fbf10ee152a30c44
Angelo Johnson <42251738+angeloplacebo@users.noreply.github.com>
2019-03-17 20:01:44 -0300

    Create Arduino_fix.sh

---
+++ scripts/Arduino_fix.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "execuntando script..."
+sleep 2
+user=`whoami`
+wget https://github.com/skyrocknroll/CH341SER_LINUX/archive/master.zip
+unzip master.zip
+cd CH341SER_LINUX-master/
+sudo make
+sudo make load
+sudo usermod -a -G tty $user
+sudo usermod -a -G dialout $user
+cd ..
+sudo rm -R CH341SER_LINUX-master
+sleep 2
+echo "finalizado"
+echo "agora salve seus documentos abertos"
+echo "e reinicie seu computador"
+slepp 1
