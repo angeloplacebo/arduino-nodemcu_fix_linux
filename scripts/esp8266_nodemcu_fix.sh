6c732097b4bfd9f945afd4d5e3b867bdaac17f0a
Angelo Johnson <42251738+angeloplacebo@users.noreply.github.com>
2019-03-17 20:39:16 -0300

    Update esp8266_nodemcu_fix.sh

--- scripts/esp8266_nodemcu_fix.sh
+++ scripts/esp8266_nodemcu_fix.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+echo "executando ..."
 user=`whoami`
 group=`groups|awk '{print $1}'`
 var1=`lsusb | grep Qin | awk '{print $6}' | awk -F ":" '{print $1}'`
 var2=`lsusb | grep Qin | awk '{print $6}' | awk -F ":" '{print $2}'`
 echo -e "SUBSYSTEM==\"tty\", GROUP=\"$user\", MODE=\"0660\" \nACTION==\"add\", SUBSYSTEMS==\"usb\", ATTRS{idVendor}==\"$var1\", ATTRS{idProduct}==\"$var2\", SYMLINK+=\"nodemcu\"" > /etc/udev/rules.d/99-nodemcu.rules
+echo "finalizado"
+echo "agora salve seus documentos abertos"
+echo "script by: @angeloplacebo"
+echo "reinicie seu computador para que as alterações tenham efeito"
+sleep 2
